daa86837ec0c3b24e7a45f1006190d242c9a456c
sergeyTabakaev <tabakaev.ca2003@gmail.com>
2025-10-22 15:00:00 +0000

    Добавлен экземпляр класса Gen типа String.

--- PR1.java
+++ PR1.java
@@ -1,11 +1,16 @@
 class Gen<T> {
+	//Declaring a general class type
 	T ob;
+	//Defining class constructor
+	//assigning generalized object as a parameter
 	Gen(T o){
 		ob = o;
 	}
+	//Method that returns class object
 	T getOb() {
 		return ob;
 	}
+	//Method that returns the name of a class type
 	void showType() {
 		System.out.println("Type of T: " + ob.getClass().getName());
 	}
@@ -18,5 +23,14 @@ class pr001 {
 		iob.showType();
 		int v = iob.getOb();
 		System.out.println("Value: " + v);
+		System.out.println("Value: " + v + "\n");
+		//Creating a class pointer with object type String
+		//Assigning a new class instance of type String to our pointer
+		Gen<String> strob = new Gen<String>("String of generalized class");
+		//Showing object Type
+		strob.showType();
+		//Assigning object value to a variable
+		String str = strob.getOb();
+		System.out.println("Value: " + str + "\n");
 	}
 }
